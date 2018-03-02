3f39c7bca9f134685df89476342ca4a8d738ab06
markokoprivica97 <markokoprivica97@gmail.com>
2018-03-02 11:36:06 +0100

    Uveden interfejs BibliotekaInterfejs
    Premestene klase Autor i Kniga

---
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -0,0 +1,12 @@
+package biblioteka.interfejs;
+
+import biblioteka.Knjiga;
+
+public interface BibliotekaInterfejs {
+
+	
+	public void dodajKnjigu(Knjiga k);
+	public void obrisiKnjigu(Knjiga k);
+	
+	
+}
