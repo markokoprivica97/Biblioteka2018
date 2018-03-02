d2314d5fa2d07b5478ddcf781f4f1f6fa93df8f9
markokoprivica97 <markokoprivica97@gmail.com>
2018-03-02 11:38:24 +0100

    Dodat interfejes

--- Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
+++ Biblioteka/src/biblioteka/interfejs/BibliotekaInterfejs.java
@@ -1,5 +1,8 @@
 package biblioteka.interfejs;
 
+import java.util.LinkedList;
+
+import biblioteka.Autor;
 import biblioteka.Knjiga;
 
 public interface BibliotekaInterfejs {
@@ -7,6 +10,9 @@ public interface BibliotekaInterfejs {
 	
 	public void dodajKnjigu(Knjiga k);
 	public void obrisiKnjigu(Knjiga k);
+	public LinkedList<Knjiga> vratiSveKnjige();
+	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac);
+	
 	
 	
 }
